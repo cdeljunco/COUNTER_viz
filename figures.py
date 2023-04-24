2c39402cd7b05f2c6abeac44c17b5180e37f8d6d
Ricardo Zamora <rzgarcia27@gmail.com>
2023-04-24 14:56:54 -0400

    minor codebase layout changes

--- figures.py
+++ figures.py
@@ -8,11 +8,11 @@ def linePlot(date_col, cpuDF):
         "Date Range": date_col,
         "Cost Per Use": cpuDF
     }
-    # create a dataframe from the given data range and the CPU per fiscal year
+    # Create a dataframe from the given data range and the CPU per fiscal year
     cpuDataFrame = pd.DataFrame(cpuData)
     fig1 = px.line(cpuDataFrame,
-               x="Date Range",
-               y="Cost Per Use")
+                    x="Date Range",
+                    y="Cost Per Use")
     return (fig1,cpuDataFrame)
 
 #create a histogram showing the distributions of Journals based on the Reporting Period Total
@@ -21,26 +21,26 @@ def histogram(dataframe,filter_max,chartHeight):
             alt.X("Reporting_Period_Total:Q", scale=alt.Scale(
                 domain=[0, filter_max]), title="Reporting Period Total"),
             alt.Y("count()",
-                  axis=alt.Axis(grid=False),
-                  title="Number of Journals"),
+                    axis=alt.Axis(grid=False),
+                    title="Number of Journals"),
             alt.Detail("Title"),
             tooltip=["Title", "Reporting_Period_Total"],
             ).interactive().configure_view(height=chartHeight)
     return stacked_hist
 
 
-#Create a bar chart with color corresponding to the fiscal years
+# Create a bar chart with color corresponding to the fiscal years
 def barChart(dataframe):
     fig = px.bar(dataframe,
-                 x="Title",
-                 y="Reporting_Period_Total",
-                 color="Fiscal Year",
-                 barmode="group",
-                 color_discrete_sequence=[
-                     '#D81B60', '#1E88E5', '#FFC107', "#004D40", "#E48DF6", "#FA01B8", "#C16B04"],
-                 labels={
-                     "Title": "Journal Title",
-                     "Reporting_Period_Total": "Reporting Period Total"
-                 })
+                    x="Title",
+                    y="Reporting_Period_Total",
+                    color="Fiscal Year",
+                    barmode="group",
+                    color_discrete_sequence=[
+                        '#D81B60', '#1E88E5', '#FFC107', "#004D40", "#E48DF6", "#FA01B8", "#C16B04"],
+                    labels={
+                        "Title": "Journal Title",
+                        "Reporting_Period_Total": "Reporting Period Total"
+                })
     return fig
 
